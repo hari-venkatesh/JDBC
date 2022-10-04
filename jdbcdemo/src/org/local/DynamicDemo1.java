9ca8e607a9cb828b360930e0dc7d4964fc014b8b
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-10-04 22:21:01 +0530

    Good Blocks of JDBC codes

---
+++ jdbcdemo/src/org/local/DynamicDemo1.java
@@ -0,0 +1,38 @@
+package org.local;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DynamicDemo1 {
+	public static void main(String[] args) throws Exception {
+		Connection conn=null;
+		PreparedStatement pst=null;
+		String qry="INSERT INTO Student VALUES(?,?,?)";
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc_demo", 
+					"root", "Sarpata@6");
+			pst=conn.prepareStatement(qry);
+			pst.setInt(1, 10);
+			pst.setString(2, "Hari");
+			pst.setInt(3, 22);
+			pst.execute();
+		}catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(conn!=null) {
+				conn.close();
+			}
+			if(pst!=null) {
+				pst.close();
+			}
+		}
+
+	}
+
+}
+			
+
+
---
+++ jdbcdemo/src/org/local/GetStudents.java
@@ -0,0 +1,52 @@
+package org.local;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class GetStudents {
+	public static void main(String[] args) throws Exception {
+		Connection conn=null;
+		Statement st=null;
+		ResultSet res=null;
+		//ResultSet ress=null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc_demo", 
+					"root", "Sarpata@6");
+			st = conn.createStatement();
+			res = st.executeQuery("SELECT * FROM Student");
+			//ress = st.executeQuery("SELECT * FROM Student");
+			System.out.println();
+			System.out.println("ID    Name    Age");
+			while(res.next()) {
+				System.out.println(res.getInt("id")+"    "+res.getString("Name")+"    "+res.getInt("Age"));
+			}
+			System.out.println();
+//			System.out.println("ID    Name    Age");
+//			while(ress.next()) {
+//				System.out.println(res.getInt(1)+"    "+res.getString(2)+"    "+res.getInt(3));
+//			}
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(conn!=null) {
+				conn.close();
+			}
+			if(st!=null) {
+				st.close();
+			}
+			if(res!=null) {
+				res.close();
+			}
+//			if(ress!=null) {
+//				ress.close();
+//			}
+		}
+
+	}
+
+
+}
